2d0f92678c5e7c92caded8d6e390000f55cf8da5
Andres Cayambe <andrescayambe99@hotmail.com>
2023-06-19 21:08:06 +0200

    [add]Delete

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -102,6 +102,8 @@ class ComicController extends Controller
      */
     public function destroy(Comic $comic)
     {
-        //
+        $comic->delete();
+
+        return redirect()->route('comics.index')->with('deleted', "il Fumetto $comic->title è stato eleiminato");
     }
 }
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,32 +1,43 @@
 @extends('layout.main')
 
 @section('content')
-
-<table class="table table-hover">
-    <thead>
-      <tr>
-        <th scope="col">#id</th>
-        <th scope="col">Titolo</th>
-        <th scope="col">Prezzo</th>
-        <th scope="col">Azione</th>
-
-
-      </tr>
-    </thead>
-    <tbody>
-        @foreach ($comics as $comic)
-        <tr>
-          <td>{{ $comic->id }}</td>
-          <td>{{ $comic->title }}</td>
-          <td>{{ $comic->price }}</td>
-          <td><a href="{{ route('comics.show', $comic) }}" class="btn btn-primary">Vai</a></td>
-          <td><a href="{{ route('comics.create', $comic) }}" class="btn btn-secondary">Nuovo</a></td>
-          <td><a href="{{ route('comics.edit', $comic) }}" class="btn btn-secondary">Modifica</a></td>
+    @if (session('deleted'))
+        <div class="alert alert-danger" role="alert">
+            {{ session('deleted') }}
+        </div>
+    @endif
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">#id</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Azione</th>
 
 
-        </tr>
-        @endforeach
-    </tbody>
-  </table>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comics as $comic)
+                <tr>
+                    <td>{{ $comic->id }}</td>
+                    <td>{{ $comic->title }}</td>
+                    <td>{{ $comic->price }}</td>
+                    <td><a href="{{ route('comics.show', $comic) }}" class="btn btn-primary">Vai</a></td>
+                    <td><a href="{{ route('comics.create', $comic) }}" class="btn btn-secondary">Nuovo</a></td>
+                    <td><a href="{{ route('comics.edit', $comic) }}" class="btn btn-secondary">Modifica</a></td>
+                    <td>
+                        <form class="d-inline" action="{{ route('comics.destroy', $comic) }}" method="POST"
+                            onsubmit="return confirm('Confermi di volerlo eliminare')">
 
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger" title="Elimina">elmina</button>
+                        </form>
+                    </td>
+
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
